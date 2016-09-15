8c2280f9caac03720ba66585b8893e1b223a2b58
App Academy Student <student@appacademy.io>
2016-09-15 16:23:28 -0700

    add neighbors_pos and neighbors to tile.rb

--- tile.rb
+++ tile.rb
@@ -11,12 +11,37 @@ class Tile
     @revealed ? puts "Tile already revealed " : @revealed == true
   end
 
-  def neighbors(pos)
+  def neighbors_pos(pos) #returns array of positions of adjacecent tiles
+    neighbors_pos = []
+    x_options = coord_generator (pos[0])
+    y_options = coord_generator (pos[1])
+    x_options.each do |x|
+      y_options.each do |y|
+        neighbors << [x,y] if [x,y] != pos && tile_exists?([x,y])
+      end
+    end
+    neighbors_pos
+  end
 
+  def neighbors(pos)
+    neighbors = []
+    neighbors_pos(pos).each do |neighbor_pos|
+      neighbors << @board[*neighbor_pos]
+    end
+    neighbors
+  end
+
+  def tile_exists?(coord)
+    return false if coord.any?{|x| x < 0 || x > @board.size}
+    true
+  end
+
+  def coord_generator(coord)
+    [coord - 1, coord, coord +1]
   end
 
   def neighbor_bomb_count(pos)
-
+    neighbors(pos).count {|neighbor| neighbor.bomb}
   end
 
   def change_status(pos, choice)
